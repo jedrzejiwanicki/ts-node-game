de3964bef3f25362537c2538fbcbed729d18e764
jedrzejiwanicki <iwanicki.jedrzej@gmail.com>
2019-03-09 10:42:22 +0100

    handle serving static to nginx

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import SocketIO from 'socket.io';
 import http from 'http';
 import cors from 'cors';
-import path from 'path';
 
 
 import { UserRouter } from './routes/User';
@@ -18,9 +17,6 @@ global.io = SocketIO(server, { serveClient: false });
 
 new GameService().init();
 
-app.use(express.static('./client/dist/'));
-
-app.use('/', (req: Request, res: Response) => res.sendFile(path.resolve('client', 'dist', 'index.html')));
 app.use(cors());
 app.use('/user', UserRouter);
 app.use('/grid', GridRouter);
