1a2db0c0e1fb24005f31d52a162dcac5c6f02bef
jedrzejiwanicki <iwanicki.jedrzej@gmail.com>
2019-03-09 13:20:57 +0100

    change socket URI

--- client/scripts/utils/Socket.ts
+++ client/scripts/utils/Socket.ts
@@ -14,7 +14,7 @@ export class Socket {
   }
 
   connect(): void {
-    this.socket = SocketIO(`${process.env.HOSTNAME}`, { path: '/api' });
+    this.socket = SocketIO(`${process.env.HOSTNAME}`, { path: '/api/socket.io' });
   }
 
   getInstance(): any {
